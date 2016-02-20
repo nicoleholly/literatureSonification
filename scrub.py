f0657810afe95db397333c06d9f14e1a76ae6715
Nicole <nhb228@nyu.edu>
2016-02-20 17:40:19 -0500

    so many changes XD

---
+++ scrub.py
@@ -0,0 +1,21 @@
+def main():
+    source_file = "books/alice.txt"
+    target_file = "books/alice_scrubbed"
+
+    s = open(source_file, 'r')
+    t = open(target_file, 'w')
+
+    countRecords = 0
+
+    for line in s:
+        currentLine = line.rstrip("\n")
+        t.write(currentLine)
+        countRecords += 1               
+
+    s.close()
+    t.close()
+
+    print("File written. %d records. " % countRecords)
+
+main()
+           
--- sentiment.js
+++ sentiment.js
@@ -9,18 +9,17 @@ file.addTrack(track);
 
 //var notes = require('books/notes-from-underground.txt');
 
-
 var score = [];
-
+var music = []; 
 //var text = "I am a sick man. I am a spiteful man. I am an unattractive man."
 
 var text = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversations?'"
-var textArray = text.split('.');
+var textArray = text.split(',');
 console.log(textArray);
 
-var blah = '';
 
 for (i =0; i < textArray.length; i++) {
+	var blah = ''
 	score[i] = sentiment(textArray[i]).score;
 	if (score[i] % 7 == 0) {
 		blah += 'c';
@@ -39,17 +38,41 @@ for (i =0; i < textArray.length; i++) {
 	} else {
 		console.log('wut');
 	}
+
+	if (score[i] < -25) {
+		blah += 0;
+	} else if (score[i] < -20) {
+		blah += 1;
+	} else if (score[i] < -15) {
+		blah += 2;
+	} else if (score[i] < -10) {
+		blah += 3;
+	} else if (score[i] < -5) {
+		blah += 4;
+	} else if (score[i] < 0) {
+		blah += 5;
+	} else if (score[i] < 5) {
+		blah += 6;
+	} else if (score[i] < 10) {
+		blah += 7;
+	} else if (score[i] < 15) {
+		blah += 8;
+	} else if (score[i] < 20) {
+		blah += 9;
+	} else {
+		blah += 10;
+	}
+
+	music.push(blah);
 }
-
-
 
 console.log(score);
 console.log(blah);
-/*
-track.addNote(0, 'c4', 64);
-track.addNote(0, 'd4', 64);
-track.addNote(0, 'e4', 64);
+console.log(music);
+
+for (var j = 0; j < music.length; j++ ) {
+	track.addNote(0, music[j], 64);
+};
 
 fs.writeFileSync('test.mid', file.toBytes(), 'binary');
-*/
 
