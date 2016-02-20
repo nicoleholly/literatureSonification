3e170a25c6f416d8a10669a1c0c7aea061196671
Nicole <nhb228@nyu.edu>
2016-02-20 15:13:10 -0500

    beginnings of da algorithm \o\ /o/ \o/

--- sentiment.js
+++ sentiment.js
@@ -1,2 +1,32 @@
 var sentiment = require('sentiment');
-var datap = require('data-piano')
+var datap = require('data-piano');
+var baudio = require('baudio');
+//var notes = require('books/notes-from-underground.txt');
+
+
+var score = [];
+var text = "I love feelings really happy. I hate sad things."
+textArray = text.split('.');
+
+console.log(textArray);
+
+for (i =0; i < textArray.length; i++) {
+	score[i] = sentiment(textArray[i]).score; 
+}
+
+console.log(score);
+
+
+var piano = new datap({
+	data: score,
+	velocityData: score,
+	lowKey: 60,
+	highKey: 71,
+	lowVelocity: 42,
+	highVelocity: 80,
+	stopVelocity: 80
+})
+
+var playFunc = piano.getSinPlayFunc(2);
+var b = baudio(playFunc);
+b.play();
